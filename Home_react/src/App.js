d556c4e90a7345f917beb6a3fcaaacebf1d3eee2
Saurabh Shewale_March22 <saurabhshewale70@gmail.com>
2022-09-18 01:28:15 +0530

    Saurabh
    Footer and some mods

--- Home_react/src/App.js
+++ Home_react/src/App.js
@@ -8,7 +8,7 @@ import Header from './components/Header';
 import Howitworks from './components/Howitworks'
 import About from './components/About';
 import Properties from './components/Properties';
-import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 
 
@@ -17,11 +17,13 @@ import Contact from './components/Contact';
 function App() {
     return ( 
         <>
+    
          <Header/>
          <Howitworks/>
          <About/>
          <Properties/>
-         <Contact/>
+         <Footer/>
+         
          
         </>
         
--- Home_react/src/components/Properties.jsx
+++ Home_react/src/components/Properties.jsx
@@ -15,6 +15,9 @@ function Properties() {
             <Bproperty image={pimage1} name="Flora-Heights" price="66-75L"/>
             <Bproperty image={pimage2} name="Finswell-Woods" price="98-99L"/>
             <Bproperty image={pimage3} name="Sky-Lakes" price="1.5-2CR"/>
+            <Bproperty image={pimage3} name="Bellmonte" price="40-50L"/>
+            <Bproperty image={pimage3} name="Sky-Magenta" price="1.2-1.3CR"/>
+            <Bproperty image={pimage3} name="Platinum-Towers" price="1.0-1.1CR"/>
         </div>
     </div>
   )
