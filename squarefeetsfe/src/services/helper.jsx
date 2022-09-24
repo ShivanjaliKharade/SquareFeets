3e4cb5f5a1b892fbd71c7cc25511d52e9f53fd28
ShivanjaliKharade <100847701+ShivanjaliKharade@users.noreply.github.com>
2022-09-25 00:03:06 +0530

    Delete squarefeetsfe directory

--- squarefeetsfe/src/services/helper.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-export const BASE_URL = "http://localhost:9292"; //here we use server api port number
-
-//provide object of Axios
-//we can able to use server
-export const myAxios = axios.create({
-  baseURL: BASE_URL, //key value pair
-});
